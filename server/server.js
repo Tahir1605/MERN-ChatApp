21fa35c92d1f78b15982254d18f46cb60e95e844
Tahirul Islam <5566tahirul@gmail.com>
2025-06-12 23:16:32 +0530

    server.js change

--- server/server.js
+++ server/server.js
@@ -16,42 +16,45 @@ const server = http.createServer(app)
 
 // initialize socket.io server
 
-export const io = new Server(server,{
-    cors:{origin:"*"}
+export const io = new Server(server, {
+    cors: { origin: "*" }
 })
 // Store online Users
 
-export const userSocketMap = {}; 
+export const userSocketMap = {};
 
 // Socket.io connection handler
 
-io.on("connection",(socket) => {
+io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
-    console.log("User Connected", userId);  
+    console.log("User Connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
+    if (userId) userSocketMap[userId] = socket.id;
     // Emit online users to all connected clients
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    socket.on("disconnect" , () => {
-        console.log("User Disconnected",userId);
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
         delete userSocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
     })
 })
 
 // Middleware setup
 
-app.use(express.json({limit:"4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 // Routes setup
-app.use("/api/status", (req,res) => res.send("Server is live"));
-app.use("/api/auth",userRouter);
-app.use("/api/messages",messageRouter);
+app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 
 // connect to mongodb
 
 await connectDB();
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT : "+PORT));
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on PORT : " + PORT));
+}
+// Export server for versel
+export default server;
